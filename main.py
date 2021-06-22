20476c86990aa89836e388776726cbf3e7c94477
hamdifu <mainianshika@gmail.com>
2021-06-23 01:39:31 +0530

    change

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm,RegisterForm,LoginForm,CommentForm
 from functools import wraps
 from flask_gravatar import Gravatar
-import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ['SECRETKEY']
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
